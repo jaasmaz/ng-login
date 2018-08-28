70fe65dc31bb43064aacda9290b558940b726c38
jaasmaz <jaasmaz@yahoo.com>
2018-08-27 23:01:00 -0700

    changed app.component.ts to reactive form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms'
 
 @Component({
   selector: 'app-root',
@@ -6,6 +7,44 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.sass']
 })
 export class AppComponent {
-  title = 'app';
-  log(x){console.log(x)}
+  
+  rForm: FormGroup;
+  post:any;                // for the submitted form
+  description:string = '';
+  name:string = '';
+  titleAlert:string = "This is Required";
+  constructor(private fb: FormBuilder) {
+
+    this.rForm = fb.group({
+      'name': [null, Validators.required],
+      'description': [null, Validators.compose([Validators.required, Validators.minLength(10), Validators.maxLength(200)])],
+      'validate' : ''
+    });
+
+    console.log(this.rForm)
+
+  }
+
+    ngOnInit() {
+      this.rForm.get('validate').valueChanges.subscribe(
+        (validate) => {
+          if (validate == '1') {
+            this.rForm.get('name').setValidators([Validators.required, Validators.minLength(3)]);
+            this.titleAlert = "You Need to put at leat 3 chars";
+          } else {
+            this.rForm.get('name').setValidators(Validators.required);
+          }
+        this.rForm.get('name').updateValueAndValidity();
+        }
+      )
+
+    }
+
+  addPost(post){
+    this.description = post.description;
+    this.name = post.name;
+  }
+
 }
+
+
